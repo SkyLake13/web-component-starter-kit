813c7ee18fe9be09d0e848846eefd845b069253b
kumawabh <kumawabh@WL350183>
2020-07-02 12:48:37 +0530

    removed unused code

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,5 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
